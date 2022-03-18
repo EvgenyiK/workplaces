e27ec787cdeabe771e060a4d9019aa9d3505b268
evgen <evgenprogrammer@gmail.com>
2022-03-18 10:58:33 +0300

    add config

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+	"workplaces/internals/cfg"
+
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
-	
+	config := cfg.LoadConfig()
+	ctx, cancel := context.WithCancel(context.Background())
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	server := app.NewServer(config, ctx)
+
+	go func() { 
+		oscall := <-c 
+		log.Printf("system call:%+v", oscall)
+		server.Shutdown() 
+		cancel() 
+	}()
+
+	server.Serve()
 }
